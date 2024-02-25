87626016d9bf3bee4156daf6ddf97080ed3a81d9
kgarchie <archiethebig@gmail.com>
2024-02-25 15:14:50 +0300

    Hot-Fix for default api port

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE || 'http://api:8000'
+      apiBase: process.env.API_BASE || 'http://localhost:8000'
     }
   },
   modules: ['@nuxtjs/tailwindcss']
